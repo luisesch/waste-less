0a868878bf3ceb13828980ca0f6be43fd2d56718
Luise Schneider <luise.schneider@berlin.de>
2019-03-07 21:30:16 +0100

    started authentication

---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: String,
+    password: String,
+    email: String,
+    motto: String,
+    photo: { type: String, default: "/images/default_profile.jpg" },
+    googleID: String,
+    leagues: [{ type: Schema.Types.ObjectId, ref: "Travel" }],
+    score: Number,
+    status: {
+      type: String,
+      enum: ["Pending Confirmation", "Active"],
+      default: "Pending Confirmation"
+    },
+    confirmationCode: String
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
