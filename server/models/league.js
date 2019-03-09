3d3f70c0d1a95f47d58f54a64dbaca5ebf475de2
Luise Schneider <luise.schneider@berlin.de>
2019-03-09 17:38:23 +0100

    created functionality to create new team

--- server/models/league.js
+++ server/models/league.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const leagueSchema = new Schema({
-  id: String,
-  name: String,
-  createdBy: [{ type: Schema.Types.UserId, ref: "User" }],
-  status: {
-    type: String,
-    enum: ["active", "completed"],
-    default: "active"
+const leagueSchema = new Schema(
+  {
+    id: String,
+    name: String,
+    administrator: { type: Schema.Types.ObjectId, ref: "User" },
+    status: {
+      type: String,
+      enum: ["active", "completed"],
+      default: "active"
+    }
   },
-  timestamps: { createdAt: "created_at", expiredAt: "expired_at" }
-});
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+  }
+);
 
 const League = mongoose.model("League", leagueSchema);
 
